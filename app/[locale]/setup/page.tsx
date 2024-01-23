99416da4421e4c79d57acad138d60f3fe036b390
Kyle Mayer <kyle@mintunmedia.com>
2024-01-22 17:02:58 -0800

    init

--- app/[locale]/setup/page.tsx
+++ app/[locale]/setup/page.tsx
@@ -160,7 +160,7 @@ export default function SetupPage() {
           <StepContainer
             stepDescription="Let's create your profile."
             stepNum={currentStep}
-            stepTitle="Welcome to Chatbot UI"
+            stepTitle="Welcome to Chatt"
             onShouldProceed={handleShouldProceed}
             showNextButton={!!(username && usernameAvailable)}
             showBackButton={false}
